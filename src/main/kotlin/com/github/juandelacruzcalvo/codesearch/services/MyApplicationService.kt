89e441376048c212de0d0742432e6f48256ed8c8
GitHub Action <action@github.com>
2022-02-04 15:39:10 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/juandelacruzcalvo/codesearch/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.juandelacruzcalvo.codesearch.services
+
+import com.github.juandelacruzcalvo.codesearch.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/test/kotlin/com/github/juandelacruzcalvo/codesearch/MyPluginTest.kt
+++ src/test/kotlin/com/github/juandelacruzcalvo/codesearch/MyPluginTest.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.juandelacruzcalvo.codesearch
 
 import com.intellij.ide.highlighter.XmlFileType
 import com.intellij.psi.xml.XmlFile
